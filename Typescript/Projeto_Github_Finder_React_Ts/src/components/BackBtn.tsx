54288009c7124526903bb5b70b1376b138c3e2ec
antonio marinho <ant.marinho@outlook.com>
2024-04-10 16:15:38 -0300

    projeto github finder concluido

--- Typescript/Projeto_Github_Finder_React_Ts/src/components/BackBtn.tsx
+++ Typescript/Projeto_Github_Finder_React_Ts/src/components/BackBtn.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import classes from './BackBtn.module.css'
 
 const BackBtn = () => {
 
@@ -6,7 +7,7 @@ const BackBtn = () => {
 
   return (
     <>
-        <button onClick={() => navigate(-1)}>Voltar</button>
+        <button className={classes.back_btn} onClick={() => navigate(-1)}>Voltar</button>
     </>
   )
 }
